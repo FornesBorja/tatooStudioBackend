7353f8e240df10fc88ec47e558dac3fc211dedc7
FornesBorja <fornes.borja@gmail.com>
2024-07-12 14:55:44 +0200

    feat: dbSeeder successfully implemented

--- src/database/seeders/dbSeeder.ts
+++ src/database/seeders/dbSeeder.ts
@@ -1,8 +1,10 @@
+import serviceSeeder from "./dbServices";
 import roleSeeder from "./roleSeeder";
 
 (async () => {
     console.log("Starting seeders...");
     await roleSeeder();
+    await serviceSeeder();
  })();
  
  
