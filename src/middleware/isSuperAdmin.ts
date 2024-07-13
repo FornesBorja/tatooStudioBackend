95a4350dc2025970bf9679e79f49ba2941926285
“FornesBorja” <fornes.borja@gmail.com>
2024-07-14 01:10:42 +0200

    fix: role_id now goes by number

--- src/middleware/isSuperAdmin.ts
+++ src/middleware/isSuperAdmin.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 export const isSuperAdmin = (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.tokenData.role)
-    if(req.tokenData.role !== 1) {
+    console.log(req.tokenData.role_id)
+    if(req.tokenData.role_id !== 1) {
       return res.status(403).json(
         {
           success: false,
