2df574b39fc85da793b0d948e12e0c02bf2fb5ab
“FornesBorja” <fornes.borja@gmail.com>
2024-07-14 01:03:00 +0200

    fix: role now goes by id

--- src/middleware/isSuperAdmin.ts
+++ src/middleware/isSuperAdmin.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export const isSuperAdmin = (req: Request, res: Response, next: NextFunction) => {
   try {
     console.log(req.tokenData.role)
-    if(req.tokenData.role !== "super_admin") {
+    if(req.tokenData.role !== 1) {
       return res.status(403).json(
         {
           success: false,
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,6 @@
 export interface TokenDecoded {
     id: number,
-    role: string,
+    role: number,
     email: string
   }
   
